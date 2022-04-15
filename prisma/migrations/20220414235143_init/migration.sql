e99767ca496060dc45c02b8da0ae30ce8c60215f
Lizard <lizard@lizard.space>
2022-04-14 20:43:34 -0400

    basic api that returns users count

--- prisma/migrations/20220414235143_init/migration.sql
+++ prisma/migrations/20220414235143_init/migration.sql
@@ -1,5 +1,3 @@
--- Your SQL goes here
-
 -- CreateEnum
 CREATE TYPE "UserBadge" AS ENUM ('PREMIUM', 'MATURE', 'NOT_SAFE', 'SAFE', 'TRUSTED', 'MODERATOR', 'ADMINISTRATOR', 'SUPERADMIN');
 
@@ -32,204 +30,197 @@ CREATE TYPE "ModeratorNotificationsType" AS ENUM ('NEW_REPORT', 'SUSPICIOUS_ACTI
 
 -- CreateTable
 CREATE TABLE "Group" (
-                         "id" TEXT NOT NULL,
-                         "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                         "updatedAt" TIMESTAMP(3) NOT NULL,
-                         "name" TEXT NOT NULL,
-                         "displayName" TEXT NOT NULL,
-                         "userId" TEXT,
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "displayName" TEXT NOT NULL,
+    "userId" TEXT,
 
-                         CONSTRAINT "Group_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Group_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "GroupMember" (
-                               "id" TEXT NOT NULL,
-                               "role" "GroupMemberRole" NOT NULL DEFAULT E'NONE',
-                               "groupId" TEXT NOT NULL,
-                               "userId" TEXT NOT NULL,
-                               "invitedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "id" TEXT NOT NULL,
+    "role" "GroupMemberRole" NOT NULL DEFAULT E'NONE',
+    "groupId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "invitedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-                               CONSTRAINT "GroupMember_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "GroupMember_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "User" (
-                        "id" TEXT NOT NULL,
-                        "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                        "updatedAt" TIMESTAMP(3) NOT NULL,
-                        "username" TEXT NOT NULL,
-                        "nickname" TEXT,
-                        "bio" TEXT,
-                        "role" "UserRole" NOT NULL DEFAULT E'NONE',
-                        "email" TEXT NOT NULL,
-                        "hashedPassword" TEXT,
-                        "avatarUrl" TEXT,
-                        "avatarSourceId" TEXT,
-                        "bannerSourceId" TEXT,
-                        "badges" "UserBadge"[],
-                        "lockStatus" "LockingStatus" NOT NULL DEFAULT E'NONE',
-                        "premiumFeatures" "PremiumFeature"[],
-                        "userFavouriteIds" TEXT[],
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "username" TEXT NOT NULL,
+    "nickname" TEXT,
+    "bio" TEXT,
+    "role" "UserRole" NOT NULL DEFAULT E'NONE',
+    "permissions" INTEGER NOT NULL DEFAULT 130048,
+    "email" TEXT NOT NULL,
+    "hashedPassword" TEXT,
+    "avatarUrl" TEXT,
+    "avatarSourceId" TEXT,
+    "bannerExt" TEXT NOT NULL DEFAULT E'png',
+    "badges" "UserBadge"[],
+    "lockStatus" "LockingStatus" NOT NULL DEFAULT E'NONE',
+    "premiumFeatures" "PremiumFeature"[],
+    "userFavouriteIds" TEXT[],
 
-                        CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "UserConnection" (
-                                  "type" "UserConnectionType" NOT NULL,
-                                  "email" TEXT NOT NULL,
-                                  "handle" TEXT,
-                                  "accessToken" TEXT,
-                                  "refreshToken" TEXT,
-                                  "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                                  "updatedAt" TIMESTAMP(3) NOT NULL,
-                                  "expiresAt" TIMESTAMP(3),
-                                  "userId" TEXT NOT NULL,
-
-                                  CONSTRAINT "UserConnection_pkey" PRIMARY KEY ("userId", "email")
+    "type" "UserConnectionType" NOT NULL,
+    "email" TEXT NOT NULL,
+    "handle" TEXT,
+    "accessToken" TEXT,
+    "refreshToken" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "expiresAt" TIMESTAMP(3),
+    "userId" TEXT NOT NULL
 );
 
 -- CreateTable
 CREATE TABLE "Session" (
-                           "id" TEXT NOT NULL,
-                           "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                           "updatedAt" TIMESTAMP(3) NOT NULL,
-                           "expiresAt" TIMESTAMP(3),
-                           "handle" TEXT NOT NULL,
-                           "hashedSessionToken" TEXT,
-                           "antiCSRFToken" TEXT,
-                           "publicData" TEXT,
-                           "privateData" TEXT,
-                           "userId" TEXT,
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "expiresAt" TIMESTAMP(3),
+    "handle" TEXT NOT NULL,
+    "hashedSessionToken" TEXT,
+    "antiCSRFToken" TEXT,
+    "publicData" TEXT,
+    "privateData" TEXT,
+    "userId" TEXT,
 
-                           CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Token" (
-                         "id" TEXT NOT NULL,
-                         "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                         "updatedAt" TIMESTAMP(3) NOT NULL,
-                         "hashedToken" TEXT NOT NULL,
-                         "type" TEXT NOT NULL,
-                         "expiresAt" TIMESTAMP(3) NOT NULL,
-                         "sentTo" TEXT NOT NULL,
-                         "userId" TEXT NOT NULL,
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "hashedToken" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+    "sentTo" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
 
-                         CONSTRAINT "Token_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Token_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Category" (
-                            "slug" TEXT NOT NULL,
-                            "display" TEXT NOT NULL,
-                            "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                            "updatedAt" TIMESTAMP(3) NOT NULL,
-                            "albumId" TEXT NOT NULL,
-                            "imageId" TEXT NOT NULL,
-                            "lockStatus" "LockingStatus" NOT NULL DEFAULT E'NONE',
-                            "rating" "SafetyRating" NOT NULL DEFAULT E'UNKNOWN',
+    "slug" TEXT NOT NULL,
+    "display" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "albumId" TEXT NOT NULL,
+    "imageId" TEXT NOT NULL,
+    "lockStatus" "LockingStatus" NOT NULL DEFAULT E'NONE',
+    "rating" "SafetyRating" NOT NULL DEFAULT E'UNKNOWN',
 
-                            CONSTRAINT "Category_pkey" PRIMARY KEY ("slug")
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("slug")
 );
 
 -- CreateTable
 CREATE TABLE "Image" (
-                         "id" TEXT NOT NULL,
-                         "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                         "updatedAt" TIMESTAMP(3) NOT NULL,
-                         "sourceId" TEXT NOT NULL,
-                         "colors" INTEGER[],
-                         "title" TEXT,
-                         "description" TEXT,
-                         "authorId" TEXT NOT NULL,
-                         "groupId" TEXT,
-                         "albumId" TEXT NOT NULL,
-                         "lockStatus" "LockingStatus" NOT NULL DEFAULT E'NONE',
-                         "rating" "SafetyRating" NOT NULL DEFAULT E'UNKNOWN',
-                         "userFavouriteIds" TEXT[],
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "imageExt" TEXT NOT NULL,
+    "colors" INTEGER[],
+    "title" TEXT,
+    "description" TEXT,
+    "authorId" TEXT NOT NULL,
+    "groupId" TEXT,
+    "albumId" TEXT NOT NULL,
+    "lockStatus" "LockingStatus" NOT NULL DEFAULT E'NONE',
+    "rating" "SafetyRating" NOT NULL DEFAULT E'UNKNOWN',
+    "userFavouriteIds" TEXT[],
 
-                         CONSTRAINT "Image_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Image_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Album" (
-                         "id" TEXT NOT NULL,
-                         "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                         "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                         "sourceId" TEXT NOT NULL,
-                         "colors" INTEGER[],
-                         "title" TEXT,
-                         "description" TEXT,
-                         "authorId" TEXT NOT NULL,
-                         "groupId" TEXT,
-                         "lockStatus" "LockingStatus" NOT NULL DEFAULT E'NONE',
-                         "rating" "SafetyRating" NOT NULL DEFAULT E'UNKNOWN',
-                         "userFavouriteIds" TEXT[],
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "coverExt" TEXT NOT NULL,
+    "colors" INTEGER[],
+    "title" TEXT,
+    "description" TEXT,
+    "authorId" TEXT NOT NULL,
+    "groupId" TEXT,
+    "lockStatus" "LockingStatus" NOT NULL DEFAULT E'NONE',
+    "rating" "SafetyRating" NOT NULL DEFAULT E'UNKNOWN',
+    "userFavouriteIds" TEXT[],
 
-                         CONSTRAINT "Album_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Album_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Report" (
-                          "id" TEXT NOT NULL,
-                          "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                          "albumId" TEXT,
-                          "imageId" TEXT,
-                          "userId" TEXT,
-                          "reporteeId" TEXT NOT NULL,
-                          "reason" TEXT NOT NULL,
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "albumId" TEXT,
+    "imageId" TEXT,
+    "userId" TEXT,
+    "reporteeId" TEXT NOT NULL,
+    "reason" TEXT NOT NULL,
 
-                          CONSTRAINT "Report_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Report_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Notification" (
-                                "id" TEXT NOT NULL,
-                                "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                                "type" "NotificationType" NOT NULL,
-                                "title" TEXT NOT NULL,
-                                "message" TEXT NOT NULL,
-                                "userId" TEXT,
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "type" "NotificationType" NOT NULL,
+    "title" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+    "userId" TEXT,
 
-                                CONSTRAINT "Notification_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Notification_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "ModeratorNotifications" (
-                                          "id" TEXT NOT NULL,
-                                          "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                                          "type" "ModeratorNotificationsType" NOT NULL,
-                                          "title" TEXT NOT NULL,
-                                          "message" TEXT NOT NULL,
-                                          "userId" TEXT,
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "type" "ModeratorNotificationsType" NOT NULL,
+    "title" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+    "userId" TEXT,
 
-                                          CONSTRAINT "ModeratorNotifications_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "ModeratorNotifications_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "_UserFavouriteUsers" (
-                                       "A" TEXT NOT NULL,
-                                       "B" TEXT NOT NULL,
-
-                                       CONSTRAINT "_UserFavouriteUsers_pkey" PRIMARY KEY ("A", "B")
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
 );
 
 -- CreateTable
 CREATE TABLE "_UserFavouriteImages" (
-                                        "A" TEXT NOT NULL,
-                                        "B" TEXT NOT NULL,
-
-                                        CONSTRAINT "_UserFavouriteImages_pkey" PRIMARY KEY ("A", "B")
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
 );
 
 -- CreateTable
 CREATE TABLE "_UserFavouriteAlbums" (
-                                        "A" TEXT NOT NULL,
-                                        "B" TEXT NOT NULL,
-
-                                        CONSTRAINT "_UserFavouriteAlbums_pkey" PRIMARY KEY ("A", "B")
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
 );
 
 -- CreateIndex
--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,66 @@
-use std::{io, sync::Arc};
+use std::{env, sync::Arc};
 
 use actix_cors::Cors;
 use actix_web::{
     get, middleware, route,
     web::{self, Data},
-    App, HttpResponse, HttpServer, Responder,
+    App, HttpRequest, HttpResponse, HttpServer, Responder,
 };
-use actix_web_lab::respond::Html;
-use diesel::QueryResult;
-use juniper::FieldResult;
-use juniper::http::{GraphQLRequest};
-use juniper::meta::Field;
-use serde_json::Value::Array;
+use anyhow::Result;
+use sea_orm::{entity::*, query::*, DatabaseConnection};
 
-mod lib;
-mod models;
-mod structs;
-mod controllers;
+mod entity;
 
-use controllers::album::{get_album, get_albums};
-use crate::models::album::Album;
+use entity::prelude::*;
+use serde_json::json;
+// use juniper::http::GraphQLRequest;
 
-#[route("/graphql", method = "GET", method = "POST")]
-async fn graphql(st: web::Data<Schema>, data: web::Json<GraphQLRequest>) -> impl Responder {
-    let connection = lib::connect();
-    let user = data.execute(&st, &(connection)).await;
-    HttpResponse::Ok().json(user)
+// #[route("/graphql", method = "GET", method = "POST")]
+// async fn graphql(st: web::Data<Schema>, data: web::Json<GraphQLRequest>) -> impl Responder {
+//     let connection = lib::connect();
+//     let user = data.execute(&st, &connection).await;
+//     HttpResponse::Ok().json(user)
+// }
+
+#[derive(Clone)]
+pub struct State {
+    conn: DatabaseConnection,
 }
 
-impl QueryRoot {
-    pub fn get_album(&self) -> FieldResult<Album> {
-        get_album()
-    }
-    pub fn get_albums(&self) -> FieldResult<Vec<Album>> {
-        get_albums()
+#[get("/")]
+async fn index(req: HttpRequest, data: web::Data<State>) -> impl Responder {
+    if let Ok(users) = User::find().all(&data.conn).await {
+        HttpResponse::Ok().body(users.len().to_string())
+    } else {
+        HttpResponse::NotFound().body("Error Looking up".to_string())
     }
 }
 
 #[actix_web::main]
-async fn main() -> io::Result<()> {
-    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+async fn main() -> Result<()> {
+    std::env::set_var("RUST_LOG", "debug");
+    tracing_subscriber::fmt::init();
+    dotenv::dotenv().ok();
 
-    // Create Juniper schema
-    let schema = Arc::new(create_schema());
+    let db_url = env::var("DATABASE_URL")?;
+
+    let conn = sea_orm::Database::connect(&db_url).await?;
+
+    let state = State { conn };
 
     log::info!("Starting HTTP server...");
 
     // Start HTTP server
     HttpServer::new(move || {
         App::new()
-            .app_data(Data::from(
-                Schema::new(
-                    QueryRoot,
-                    juniper::EmptyMutation::new(),
-                    juniper::EmptySubscription::new(),
-                )))
-            .service(graphql)
-            // the graphiql UI requires CORS to be enabled
+            .app_data(web::Data::new(state.clone()))
+            .service(index)
             .wrap(Cors::permissive())
             .wrap(middleware::Logger::default())
     })
-        .workers(2)
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await?;
+
+    Ok(())
 }
