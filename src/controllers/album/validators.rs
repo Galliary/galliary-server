3a05f129bb2fc050acbee43a04aa882b95c23d36
Lizard <lizard@lizard.space>
2022-04-16 14:09:33 -0400

    validator cleanup

--- src/controllers/album/validators.rs
+++ src/controllers/album/validators.rs
@@ -6,13 +6,14 @@ const ALBUM_TITLE_MIN_LENGTH: usize = 3;
 const ALBUM_DESCRIPTION_MAX_LENGTH: usize = 200;
 
 pub struct AlbumTitleValidator;
+
 impl CustomValidator<String> for AlbumTitleValidator {
     fn check(&self, value: &String) -> Result<(), String> {
         if value.len() > ALBUM_TITLE_MAX_LENGTH {
-            return Err(String::from("Album title must be less than 32 characters"));
+            return Err("Album title must be less than 32 characters".to_string());
         }
         if value.len() < ALBUM_TITLE_MIN_LENGTH {
-            return Err(String::from("Album title must be at least 3 characters"));
+            return Err("Album title must be at least 3 characters".to_string());
         }
         Ok(())
     }
@@ -22,9 +23,7 @@ pub struct AlbumDescriptionValidator;
 impl CustomValidator<String> for AlbumDescriptionValidator {
     fn check(&self, value: &String) -> Result<(), String> {
         if value.len() > ALBUM_DESCRIPTION_MAX_LENGTH {
-            return Err(String::from(
-                "Album description must be less than 200 characters",
-            ));
+            return Err("Album description must be less than 200 characters".to_string());
         }
         Ok(())
     }
